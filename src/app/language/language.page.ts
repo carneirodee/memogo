d6a3ea1c25f4c0497fbe8accae0184c10c0ea9f0
Deyse Carneiro <deysedayane.o.c@gmail.com>
2021-10-11 14:40:46 -0300

    Load data from Json

--- src/app/language/language.page.ts
+++ src/app/language/language.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { LoadingController } from '@ionic/angular';
 @Component({
   selector: 'app-language',
   templateUrl: './language.page.html',
@@ -7,11 +8,47 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class LanguagePage implements OnInit {
   public language: string;
+  public data : any[];
+  constructor(private activatedRoute : ActivatedRoute, public modalController: ModalController, public loadingController: LoadingController) { }
+  public isLoading;
 
-  constructor(private activatedRoute : ActivatedRoute) { }
+  async present() {
+    this.isLoading = true;
+    return await this.loadingController.create({
+      // duration: 5000,
+    }).then(a => {
+      a.present().then(() => {
+        console.log('presented');
+        if (!this.isLoading) {
+          a.dismiss().then(() => console.log('abort presenting'));
+        }
+      });
+    });
+  }
+
+  async dismiss() {
+    this.isLoading = false;
+    return await this.loadingController.dismiss().then(() => console.log('dismissed'));
+  }
+
+  // async presentModal() {
+  //   const modal = await this.modalController.create({
+  //     component: ModalPage,
+  //     cssClass: 'my-custom-class'
+  //   });
+  //   return await modal.present();
+  // }
 
   ngOnInit() {
+    this.present();
     this.language = this.activatedRoute.snapshot.paramMap.get('id');
+    fetch('./assets/data/deutsch.json').then(res => res.json())
+    .then(json => {
+      this.dismiss();
+      this.data = json;
+      this.data = Object.keys(this.data).map(e=>this.data[e]);
+      console.log("JSON", this.data)
+    });
   }
 
 }
